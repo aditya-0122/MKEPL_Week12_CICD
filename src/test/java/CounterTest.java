4a128ae15c6fa209c25e2f9ac98763362e131939
aditya-0122 <aaaaadit2021@gmail.com>
2026-06-05 19:40:03 +0700

    restore

---
+++ src/test/java/CounterTest.java
@@ -0,0 +1,52 @@
+package src.test.java;
+
+import static org.junit.Assert.*;
+import src.main.java.*;
+import org.junit.Test;
+
+public class CounterTest {
+
+	@Test
+	public void testReset() {
+		Counter testCounter = new Counter();
+		
+		for(int i = 0; i <10; i++) {
+			testCounter.increment();
+		}
+		
+		testCounter.reset();
+		
+		assertEquals(testCounter.getCount(), 0);
+	}
+
+	@Test
+	public void testIncrement() {
+		Counter testCounter = new Counter();
+		
+		for(int i = 1; i <10; i++) {
+			testCounter.increment();
+			assertEquals(testCounter.getCount(), i);
+		}
+	}
+
+	@Test
+	public void testDecrement() {
+		Counter testCounter = new Counter();
+		
+		for(int i = 1; i <10; i++) {
+			testCounter.decrement();
+			assertEquals(testCounter.getCount(), i * -1);
+		}
+	}
+	
+	@Test
+	public void testPowerBy() {
+		Counter testCounter = new Counter();
+		
+		testCounter.increaseBy(2);
+		testCounter.powerBy(3);
+		
+		assertEquals(testCounter.getCount(), 8);
+	}
+
+}
